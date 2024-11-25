f1826ebc50c10b9c3cd4f2e2b213553a6175574f
badwaikparas <111993734+badwaikparas@users.noreply.github.com>
2024-11-25 16:51:52 +0530

    UI problem of no map interaction solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,66 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import hamburger from './assets/bars-solid.svg'
 import search from './assets/magnifying-glass-solid.svg'
+import Map from './Map';
+
+
+
+function extractLatLong(coordinates) {
+    const [latitude, longitude] = coordinates.split(',').map(coord => parseFloat(coord.trim()));
+    return { latitude: latitude, longitude: longitude };
+}
+
 function App() {
     const [clicked, setClicked] = useState(false);
+    const [text, setText] = useState("");
+    const [latitude, setLatitude] = useState(21.1458);
+    const [longitude, setLongitude] = useState(79.0882);
+
+    useEffect(() => {
+        console.log(latitude)
+        console.log(longitude)
+    }, [latitude, longitude])
+
     return (
-        <div className='flex justify-center' style={{ height: '100vh', width: '100vw', display: 'flex' }}>
-            <img src="" alt="map" className='absolute' />
-            <div className='header flex justify-evenly' style={{ gap: '50px', float: 'right', width: '100%' }}>
-                <div className='searchBar m-5 border-blue-400 border-2' style={{ background: 'red', width: '30%', height: '6vh', borderRadius: '50px', display: 'flex' }}>
-                    <input className='p-5 h-auto ' type="text" placeholder='Search' style={{ width: '100%', borderRadius: '50px 0 0 50px' }} />
-                    <img className='w-5 mx-4' src={search} alt="search icon" />
+        <>
+            <div className='flex justify-center' style={{ display: 'flex' }}>
+                <div className='w-screen h-screen absolute' style={{ zIndex: -1 }}>
+                    <Map latitude={latitude} longitude={longitude} />
                 </div>
-                <div className='layers flex absolute right-0 top-0 m-5 flex-col gap-4 items-end'
-                    onClick={() => setClicked(clicked => !clicked)}
-                >
-                    <div className='flex items-center gap-2 bg-slate-600 p-4 rounded-full'>
-                        {/* <div className=''>Layers</div> */}
-                        <img className='w-4' src={hamburger} alt="hamburger" />
+                <div className='header flex justify-evenly' style={{ gap: '50px', float: 'right', width: '100%' }}>
+                    <div className='searchBar m-5 border-blue-400 border-2 ' style={{ background: 'red', width: '30%', height: '6vh', borderRadius: '50px', display: 'flex' }}>
+                        <input className='p-5 h-auto' value={text} onChange={(e) => { setText(e.target.value) }} type="text" placeholder='Search' style={{ width: '100%', borderRadius: '50px 0 0 50px' }} />
+                        <img className='w-5 mx-4' src={search} alt="search icon"
+                            onClick={() => {
+                                const coords = extractLatLong(text);
+                                setLatitude(coords.latitude);
+                                setLongitude(coords.longitude);
+                                console.log(JSON.stringify(coords))
+                            }}
+                        />
                     </div>
-                    {
-                        clicked &&
-                        <div className='bg-slate-500 rounded-xl px-5'>
-                            <ul>
-                                <li className='p-4 border-b-2 border-blue-400 font-bold'>Layer 1</li>
-                                <li className='p-4 border-b-2 border-blue-400 font-bold'>Layer 2</li>
-                                <li className='p-4 font-bold'>Layer 3</li>
-                            </ul>
+                    <div className='layers flex absolute right-0 top-0 m-5 flex-col gap-4 items-end'
+                        onClick={() => setClicked(clicked => !clicked)}
+                    >
+                        <div className='flex items-center gap-2 bg-slate-600 p-4 rounded-full'>
+                            {/* <div className=''>Layers</div> */}
+                            <img className='w-4' src={hamburger} alt="hamburger" />
                         </div>
-                    }
+                        {
+                            clicked &&
+                            <div className='bg-slate-500 rounded-xl px-5'>
+                                <ul>
+                                    <li className='p-4 border-b-2 border-blue-400 font-bold'>Layer 1</li>
+                                    <li className='p-4 border-b-2 border-blue-400 font-bold'>Layer 2</li>
+                                    <li className='p-4 font-bold'>Layer 3</li>
+                                </ul>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
---
+++ src/Map.jsx
@@ -0,0 +1,35 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix for missing marker icons in Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+const Map = ({ latitude, longitude }) => {
+    return (
+        <div style={{ height: "100vh", width: "100vw", position: "absolute" }}>
+            <MapContainer
+                center={[latitude, longitude]}
+                zoom={13}
+                style={{ height: "100%", width: "100%" }}
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[latitude, longitude]}>
+                    <Popup>
+                        A pretty popup! <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
+};
+
+export default Map;
